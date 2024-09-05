7a02e30e4688306081f5c0ce08f74e5d1b49cc89
Nicolas Mazzon <nicolasmazzon549@gmail.com>
2024-09-05 22:18:27 +0800

    `PrefixMatcher` wasn't totally ordered.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
 use std::cmp::Ordering;
 use std::ops::Range;
 
-fn cmp(a: &str, b: &str) -> Ordering {
+#[inline]
+fn sort_cmp(a: &str, b: &str) -> Ordering {
+    for (x, y) in a.chars().zip(b.chars()) {
+        if x < y {
+            return Ordering::Less;
+        } else if x > y {
+            return Ordering::Greater;
+        }
+    }
+
+    a.len().cmp(&b.len())
+}
+
+#[inline]
+fn find_cmp(a: &str, b: &str) -> Ordering {
     for (x, y) in a.chars().zip(b.chars()) {
         if x < y {
             return Ordering::Less;
@@ -27,27 +41,26 @@ impl<Metadata> PrefixMatch<Metadata> {
 
     /// Sorts elements to allow for searching.
     pub fn reorder(&mut self) {
-        self.items.sort_unstable_by(|a, b| cmp(&a.0, &b.0));
+        self.items.sort_unstable_by(|a, b| sort_cmp(&a.0, &b.0));
     }
 
     /// Find some given prefix and return a range back into the items.
     /// Must call [`PrefixMatch::reorder`] before calling this.
     pub fn find(&self, prefix: &str) -> Match {
         // This works as cmp() will return Ordering::Equal if the prefix matches.
-        let mid = match self.items.binary_search_by(|item| cmp(&item.0, prefix)) {
-            Ok(idx) => idx,
-            Err(_) => return Match { range: 0..0 },
+        let Ok(mid) = self.items.binary_search_by(|item| find_cmp(&item.0, prefix)) else {
+            return Match { range: 0..0 };
         };
 
         // Look left and try to find more matching prefixes
         let mut start = mid;
-        while start != 0 && self.items[start - 1].0.starts_with(prefix) {
+        while start > 0 && self.items[start - 1].0.starts_with(prefix) {
             start -= 1;
         }
 
         // Look right and try to find more matching prefixes
         let mut end = mid;
-        while end != self.items.len() && self.items[end + 1].0.starts_with(prefix) {
+        while end + 1 < self.items.len() && self.items[end + 1].0.starts_with(prefix) {
             end += 1;
         }
 
